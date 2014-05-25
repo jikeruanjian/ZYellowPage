09e61277359891316bb9d1b07c129f7ed0a0b3e3
jikeruanjian <jikeruanjian@126.com>
2014-05-25 15:48:26 +0800

    Revert "首页丽人"
    This reverts commit a6695488c9dc95fa637b4bb0e144ca7e4d1a1333.

--- ZYellowPage/src/com/zdt/zyellowpage/activity/fragment/FragmentHomePage.java
+++ ZYellowPage/src/com/zdt/zyellowpage/activity/fragment/FragmentHomePage.java
@@ -433,12 +433,9 @@ public class FragmentHomePage extends Fragment implements OnClickListener {
 			mActivity.startActivity(intent);
 			break;
 		case R.id.imageButtonPeople:
-			//intent = new Intent(mActivity, PopPersonListActivity.class);
-			//intent.putExtra("Type", "个人列表");
-			//intent.putExtra("TypeId", "list-hot");
-			intent = new Intent(mActivity, PopBusinessListActivity.class);
-			intent.putExtra("Type", "丽人");
-			intent.putExtra("TypeId", "丽人");
+			intent = new Intent(mActivity, PopPersonListActivity.class);
+			intent.putExtra("Type", "个人列表");
+			intent.putExtra("TypeId", "list-hot");
 			mActivity.startActivity(intent);
 			break;
 		// 热门商家
