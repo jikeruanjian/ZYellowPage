4ab542900845a88ac1dff8f4543604946ca7f7f0
jikeruanjian <jikeruanjian@126.com>
2014-08-26 09:55:02 +0800

    附近商家接口更新

--- ZYellowPage/src/com/zdt/zyellowpage/jsonEntity/NearCompanyReqEntity.java
+++ ZYellowPage/src/com/zdt/zyellowpage/jsonEntity/NearCompanyReqEntity.java
@@ -1,5 +1,7 @@
 package com.zdt.zyellowpage.jsonEntity;
 
+import com.ab.util.AbStrUtil;
+
 /**
  * 附近商家请求参数类
  * 
@@ -9,6 +11,42 @@ package com.zdt.zyellowpage.jsonEntity;
 public class NearCompanyReqEntity {
 
 	private int page_number = 0;
+
+	private int max_size = 10;
+
+	private Double latitude;
+
+	private Double longitude;
+
+	private int distance = 1000;
+
+	private String keyWord;
+
+	public NearCompanyReqEntity(int max, Double lat, Double lon, int dis) {
+		max_size = max;
+		latitude = lat;
+		longitude = lon;
+		distance = dis;
+	}
+
+	/**
+	 * 按照分类来获取周围商家
+	 * @param max
+	 * @param lat
+	 * @param lon
+	 * @param dis
+	 * @param typeID 商家分类编号
+	 */
+	public NearCompanyReqEntity(int max, Double lat, Double lon, int dis,
+			String typeID) {
+		max_size = max;
+		latitude = lat;
+		longitude = lon;
+		distance = dis;
+		if (!AbStrUtil.isEmpty(typeID)) {
+			this.keyWord = "list-" + typeID;
+		}
+	}
 
 	public int getPage_number() {
 		return page_number;
@@ -50,17 +88,11 @@ public class NearCompanyReqEntity {
 		this.distance = distance;
 	}
 
-	private int max_size = 10;
+	public String getKeyWord() {
+		return keyWord;
+	}
 
-	private Double latitude;
-
-	private Double longitude;
-
-	private int distance = 1000;
-	public NearCompanyReqEntity(int max,Double lat,Double lon,int dis){
-		max_size = max;
-		latitude = lat;
-		longitude = lon;
-		distance = dis;
+	public void setKeyWord(String keyWord) {
+		this.keyWord = keyWord;
 	}
 }
